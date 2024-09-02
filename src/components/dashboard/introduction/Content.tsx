d0c758e5d96ff142ea2b97493b4cdebd773f7ec7
ricoadtxx <euricoadit555@gmail.com>
2024-09-02 15:22:45 +0700

    Layouting: Introduction Page

--- src/components/dashboard/introduction/Content.tsx
+++ src/components/dashboard/introduction/Content.tsx
@@ -3,50 +3,137 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
+import { Bar } from "react-chartjs-2";
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend,
+} from "chart.js";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+
+ChartJS.register(
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend,
+	ChartDataLabels
+);
 
 const ContentIntroduction = () => {
 	const { ref, inView } = useInView({
-		triggerOnce: false, // Set to false to make the animation trigger multiple times
-		threshold: 0.1, // Trigger when 10% of the element is in view
+		triggerOnce: false,
+		threshold: 0.1,
 	});
+
+	const data = {
+		labels: ["2019", "2020", "2021", "2022", "2023"],
+		datasets: [
+			{
+				label: "Persentase Penduduk Miskin",
+				data: [3.47, 4.53, 4.72, 4.69, 4.44],
+				backgroundColor: "rgba(255, 99, 132, 1)",
+				borderColor: "rgba(75, 192, 192, 1)",
+				borderWidth: 1,
+			},
+		],
+	};
+
+	const options: any = {
+		responsive: true,
+		maintainAspectRatio: false,
+		layout: {
+			padding: {
+				top: 20,
+			},
+		},
+		scales: {
+			y: {
+				beginAtZero: true,
+				ticks: {
+					callback: function (value: any) {
+						return value + "%";
+					},
+					color: "white",
+				},
+				title: {
+					color: "white",
+				},
+			},
+		},
+		plugins: {
+			legend: {
+				labels: {
+					color: "white",
+				},
+				position: "bottom",
+			},
+			tooltip: {
+				callbacks: {
+					labelTextColor: () => "white",
+				},
+			},
+			datalabels: {
+				color: "white",
+				display: true,
+				anchor: "end",
+				align: "top",
+				offset: 4,
+				font: {
+					size: 12,
+				},
+			},
+		},
+	};
 
 	return (
 		<div className="relative lg:overflow-hidden flex flex-col w-full h-full py-28 mb-20">
-			<div className="grid grid-cols-1 lg:grid-cols-2 gap-10 px-5 z-10">
-				<div className="md:w-[80%] p-4 flex items-center">
-					<h1 className="text-4xl font-sans font-bold text-lime-400 tracking-widest text-center md:text-left">
-						About Education Disparities in Jakarta
+			<div className="grid grid-cols-1 xl:grid-cols-2 gap-10 px-5 z-10">
+				{/* Title */}
+				<div className="xl:w-[75%] p-4">
+					<h1 className="text-4xl font-sans font-bold text-lime-400 tracking-widest text-center xl:text-left">
+						About Disparities in Jakarta
 					</h1>
 				</div>
-				<div className="p-4 md:w-[65%] ml-auto">
-					<p className="text-white text-lg font-sans text-justify">
-						Perbedaan kualitas dan akses pendidikan yang terjadi di berbagai
-						wilayah di Jakarta. Ketimpangan ini disebabkan oleh berbagai faktor,
-						seperti perbedaan ekonomi, infrastruktur, dan ketersediaan tenaga
-						pengajar yang berkualitas.
+				{/* Explanation */}
+				<div className="p-4 xl:w-[70%] ml-auto">
+					<p className="text-white text-base font-sans text-justify md:text-xl xl:text-lg">
+						Perbedaan yang mencolok antar wilayah sering kali terlihat dari
+						disparitas tingkat kemiskinan yang ada. Pendidikan punya peran
+						penting dalam mengurangi ketimpangan ini, karena memberikan peluang
+						yang diperlukan untuk meningkatkan kualitas hidup mereka.
 					</p>
 				</div>
-				<div className="md:w-3/4 p-4">
-					<p className="text-white text-lg font-sans text-justify">
-						Perbedaan kualitas dan akses pendidikan yang terjadi di berbagai
-						wilayah di Jakarta. Ketimpangan ini disebabkan oleh berbagai faktor,
-						seperti perbedaan ekonomi, infrastruktur, dan ketersediaan tenaga
-						pengajar yang berkualitas.
-					</p>
+				{/* Graphic Bar */}
+				<div className="xl:w-3/4 h-[280px] p-4">
+					<h1 className="text-xl font-sans font-bold text-white text-center mb-3">
+						Data Penduduk Miskin (%)
+					</h1>
+					<Bar data={data} options={options} />
 				</div>
-				<div className="p-4 md:w-2/3 ml-auto">
-					<p className="text-white text-lg font-sans text-justify">
-						Provinsi DKI Jakarta terbagi menjadi lima wilayah Kota Administrasi
-						dan satu Kabupaten Administrasi dengan luas keseluruhan wilayah
-						662,33 km²
+				{/* Conclusion */}
+				<div className="p-4 xl:w-[70%] ml-auto flex items-center">
+					<p className="text-white text-base md:text-xl xl:text-lg font-sans text-justify">
+						Data persentase penduduk miskin di Jakarta dari 2019 hingga 2023
+						menunjukkan fluktuasi yang signifikan. Setelah meningkat dari 3,47%
+						pada 2019 menjadi 4,53% pada 2020 dan 4,72% pada 2021, angka
+						tersebut sedikit menurun menjadi 4,69% pada 2022 dan 4,44% pada
+						2023. Penurunan ini menunjukkan bahwa upaya untuk mengurangi
+						kemiskinan mulai memberikan hasil, namun masih diperlukan perhatian
+						berkelanjutan untuk mencapai pengurangan yang lebih konsisten.
 					</p>
 				</div>
 			</div>
 
 			<motion.div
-				ref={ref} // Attach the ref here
+				ref={ref}
 				initial={{ y: 100 }}
-				animate={inView ? { y: -50 } : { y: 100 }} // Animate only when in view
+				animate={inView ? { y: -50 } : { y: 100 }}
 				transition={{
 					duration: 1.5,
 					type: "spring",
--- src/components/dashboard/introduction/Header.tsx
+++ src/components/dashboard/introduction/Header.tsx
@@ -6,21 +6,38 @@ const HeaderIntroduction = () => {
 	return (
 		<div className="relative z-10 h-3/4 gap-20 flex justify-center items-center pt-28 pb-44 md:pt-14">
 			<div className="overflow-hidden z-[9999] flex flex-col justify-center items-center gap-5 py-24 mt-28">
+				{/* Title Animation */}
 				<div className="overflow-hidden">
 					<motion.h1
 						initial={{ y: 100 }}
 						animate={{ y: 0 }}
-						transition={{ duration: 1.5 }}
-						className="text-white text-5xl md:text-7xl font-anton overflow-hidden"
+						transition={{
+							duration: 1.5,
+							ease: [0.6, 0.01, -0.05, 0.9],
+						}}
+						className="text-white text-5xl md:text-7xl font-anton"
 					>
 						DKI JAKARTA
 					</motion.h1>
 				</div>
-				<p className="text-white text-2xl md:text-lg font-sans w-3/4 md:w-1/2 text-center">
-					Provinsi DKI Jakarta terbagi menjadi lima wilayah Kota Administrasi
-					dan satu Kabupaten Administrasi dengan luas keseluruhan wilayah 662,33
-					km²
-				</p>
+
+				{/* Subtitle Animation */}
+				<div className="w-3/4 md:w-1/2 overflow-hidden flex justify-center items-center">
+					<motion.p
+						initial={{ x: 1000 }}
+						animate={{ x: 0 }}
+						transition={{
+							duration: 1.5,
+							delay: 0.5,
+							ease: "easeOut",
+						}}
+						className="text-white text-2xl md:text-lg font-sans text-center"
+					>
+						Provinsi DKI Jakarta terbagi menjadi lima wilayah Kota Administrasi
+						dan satu Kabupaten Administrasi dengan luas keseluruhan wilayah
+						662,33 km²
+					</motion.p>
+				</div>
 			</div>
 		</div>
 	);
