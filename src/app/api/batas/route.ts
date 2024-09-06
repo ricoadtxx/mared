98fbf0ec5ea607541ef2124e972d5f0d392426f1
ricoadtxx <euricoadit555@gmail.com>
2024-09-06 18:35:34 +0700

    Fetching: Add Data Layer SHP Jakarta

--- src/app/api/batas/route.ts
+++ src/app/api/batas/route.ts
@@ -4,14 +4,12 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
 	try {
-		// Fetch data dari tabel Jakarta
-		const { data, error } = await supabase.from("jakarta").select("*"); // Ambil semua kolom
+		const { data, error } = await supabase.from("jakarta").select("*");
 
 		if (error) {
 			throw new Error(error.message);
 		}
 
-		// Return response JSON
 		return NextResponse.json(data, { status: 200 });
 	} catch (error: any) {
 		console.error("Error fetching data from Jakarta:", error);
